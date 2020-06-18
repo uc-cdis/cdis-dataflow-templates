aeab183a6f90c30833b0e570d4af6f667a8c085f
Giang Bui <giangbui0816@gmail.com>
2020-06-18 11:27:14 -0500

    feat(setup): add get_version function

--- setup.py
+++ setup.py
@@ -2,6 +2,22 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import setuptools
+from subprocess import check_output
+
+
+def get_version():
+    try:
+        tag = check_output(
+            ["git", "describe", "--tags", "--abbrev=0", "--match=[0-9]*"]
+        )
+        return tag.decode("utf-8").strip("\n")
+    except Exception:
+        raise RuntimeError(
+            "The version number cannot be extracted from git tag in this source "
+            "distribution; please either download the source from PyPI, or check out "
+            "from GitHub and make sure that the git CLI is available."
+        )
+
 
 REQUIRED_PACKAGES = [
     "google-cloud-storage==1.28.0",
@@ -10,7 +26,7 @@ REQUIRED_PACKAGES = [
 ]
 
 PACKAGE_NAME = "bucket_manifest"
-PACKAGE_VERSION = "0.0.1"
+PACKAGE_VERSION = get_version()
 setuptools.setup(
     name=PACKAGE_NAME,
     version=PACKAGE_VERSION,
